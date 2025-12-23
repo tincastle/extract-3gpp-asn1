167d9a6bb33e2f041fe3a83de0cb412be6381175
Seokseong Jeon <sjeon87@gmail.com>
2025-12-24 01:04:43 +0900

    Add support reading piped stdin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser};
 use extract_3gpp_asn1::extract_asn1_blocks;
+use std::io::{Read, IsTerminal};
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None, after_help = "Note: Input can be provided via a file path or standard input (stdin) with pipe (|).")]
 struct Cli {
     /// The file to process
     #[arg(value_name = "FILE")]
-    path: std::path::PathBuf,
+    path: Option<std::path::PathBuf>,
 }
 
 fn main() {
     let cli = Cli::parse();
-    let content = std::fs::read_to_string(cli.path).expect("could not read file");
+    let content = if !std::io::stdin().is_terminal() {
+        let mut buffer = String::new();
+        std::io::stdin()
+            .read_to_string(&mut buffer)
+            .expect("could not read stdin");
+        buffer
+    } else if let Some(path) = cli.path {
+        std::fs::read_to_string(path).expect("could not read file")
+    } else {
+        let _ = Cli::command().print_help();
+        std::process::exit(1);
+    };
     let extracted = extract_asn1_blocks(&content);
     println!("{}", extracted);
 }
